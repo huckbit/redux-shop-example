a3636c553fb0b223084e9d4ac51cc422221aed98
Massimiliano Ranauro <huckbit@gmail.com>
2021-11-07 19:06:39 +0000

    logger logger and composeEnhancers added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import { reducers } from "./reducers/index";
+import { logger } from "redux-logger";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducers,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducers, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
